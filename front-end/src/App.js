98dddd28cdf76a477c45bceaee932e9d9b745ff0
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-07-07 02:20:51 -0400

    fix: updated package.json libraries

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,38 +1,37 @@
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
-import logo from './media/record.png'
+import logo from './media/record.png';
 import { Songs } from './Songs/Songs';
 import { Song } from './Song/Song';
 import { New } from './New/New';
 import { Edit } from './Edit/Edit';
 import { Error } from './Error/Error';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function App() {
-  const navigate = useNavigate()
-  const homePage = () => navigate('/songs')
-  const newPage = () => navigate('/songs/new')
+  const navigate = useNavigate();
+  const homePage = () => navigate('/songs');
+  const newPage = () => navigate('/songs/new');
   return (
-        <div className="App">
-          <nav id="topNav">
-            <img src={logo} />
-            <h1>Tuner</h1>
-            <button
-            id="newButton"
-            onClick={() => newPage()}
-            >New</button>
-            <button
-            onClick={() => homePage()}
-            >Songs</button>
-          </nav>
-          <Routes>
-            <Route path='/songs' element={<Songs/>}/>
-            <Route path='/songs/:id' element={<Song/>}/>
-            <Route path='/songs/new' element={<New/>}/>
-            <Route path='/songs/:id/edit' element={<Edit/>}/>
-            <Route path='*' element={<Error/>}/>
-          </Routes>
-        </div>
+    <div className='App'>
+      <p>fdsaf</p>
+      <nav id='topNav'>
+        <img src={logo} alt='logo' />
+        <h1>Tuner</h1>
+        <button id='newButton' onClick={() => newPage()}>
+          New
+        </button>
+        <button onClick={() => homePage()}>Songs</button>
+      </nav>
+      <Routes>
+        <Route path='/songs' element={<Songs />} />
+        <Route path='/songs/:id' element={<Song />} />
+        <Route path='/songs/new' element={<New />} />
+        <Route path='/songs/:id/edit' element={<Edit />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </div>
   );
 }
 
--- front-end/src/Error/Error.js
+++ front-end/src/Error/Error.js
@@ -1,5 +1,5 @@
+import React from 'react';
 
-
-export function Error(){
-    return(<h1>Error 404</h1>)
+export function Error() {
+  return <h1>Error 404</h1>;
 }
